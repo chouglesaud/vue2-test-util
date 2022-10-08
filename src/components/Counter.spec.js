5d54710c2ddb2ffa0a3634150721fac8df8403db
chouglesaud <saudchougle.sc@gmail.com>
2022-10-08 21:04:59 +0530

    test setup

--- src/components/Counter.spec.js
+++ src/components/Counter.spec.js
@@ -1,20 +1,36 @@
 /**
  * @jest-environment jsdom
  */
-import { mount, createLocalVue } from "@vue/test-utils";
-import Counter from "./Counter.vue";
+import { mount, createLocalVue, Wrapper } from '@vue/test-utils';
+import Counter from './Counter.vue';
 
 const localVue = createLocalVue();
 
-describe("Counter.vue", () => {
+describe('Counter.vue', () => {
+  /**
+   * @type {Wrapper}
+   */
   let wrapper;
+
   beforeAll(() => {
     wrapper = null;
   });
 
-  it("Should mount Counter component", () => {
-    wrapper = mount(Counter, localVue);
+  it('Should mount Counter component', () => {
+    wrapper = mount(Counter, { localVue });
   });
 
-  it("Should increament the count when click increament button", () => {});
+  it('Should increament the count when click increament button', async () => {
+    expect(wrapper.findComponent("[data-test='count']").text()).toEqual('0');
+
+    await wrapper.findComponent("[data-test='increament-button']").trigger('click');
+
+    expect(wrapper.findComponent("[data-test='count']").text()).toEqual('1');
+  });
+
+  it('Should set the count 0 when click reset button', async () => {
+    await wrapper.findComponent("[data-test='reset-button']").trigger('click');
+
+    expect(wrapper.findComponent("[data-test='count']").text()).toEqual('0');
+  });
 });
--- src/helper.spec.js
+++ src/helper.spec.js
@@ -1,7 +1,7 @@
-import { sum } from "./helper";
+import { sum } from './helper';
 
-describe("helper.js", () => {
-  it(" 2 + 2 should be 4 ", () => {
+describe('helper.js', () => {
+  it(' 2 + 2 should be 4 ', () => {
     expect(sum(2, 2)).toEqual(4);
   });
 });
